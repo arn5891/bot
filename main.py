61034e77e1c0a832cc6a0e0cbf39173896e42231
arn5891 <118304272+arn5891@users.noreply.github.com>
2023-07-07 17:08:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import discord
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} online')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello, '+ str(message.author))
+    else:
+        await message.channel.send('idk what that was bruh what did you say')
+
+client.run(os.getenv("token"))
